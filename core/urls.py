0e7b070b2d6e47f96efe795b0962eb6f0e30554b
Sarobidy1215 <sarobidyevasoa@gmail.com>
2026-05-12 07:58:17 +0100

    fix admin inline crash causing 500 error

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,42 @@
-from django.http import HttpResponse
+from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
-def test(request):
-    return HttpResponse("APP OK")
+from cotation.views import (
+    index,
+    generer_pdf_devis,
+)
 
 urlpatterns = [
-    path('', test),
+
+    # =========================
+    # PAGE D'ACCUEIL
+    # =========================
+    path('', index, name='index'),
+
+    # =========================
+    # LOGIN
+    # =========================
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='registration/login.html'
+        ),
+        name='login'
+    ),
+
+    # =========================
+    # ADMIN
+    # =========================
+    path('admin/', admin.site.urls),
+
+    # =========================
+    # PDF
+    # =========================
+    path(
+        'devis-pdf/<int:resultat_id>/',
+        generer_pdf_devis,
+        name='generer_pdf_devis'
+    ),
+
 ]
