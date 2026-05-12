7c2607e9a61608b1b47bdd649364094694d41eae
Sarobidy1215 <sarobidyevasoa@gmail.com>
2026-05-12 06:37:03 +0100

    Préparation déploiement

--- core/settings.py
+++ core/settings.py
@@ -4,6 +4,9 @@ Django settings for core project.
 
 from pathlib import Path
 import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+ALLOWED_HOSTS = ['*']
 
 # =========================
 # BASE DIRECTORY
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
